2397d9e028ff5bfb6626c33889321b18557ca08b
佐藤里桜 <satorio@MacBook-Air-de-pete.local>
2025-12-19 16:40:59 +0900

    delete: デバック用コメント

--- badminton-reserve/src/app/layout.tsx
+++ badminton-reserve/src/app/layout.tsx
@@ -14,19 +14,6 @@ export default function RootLayout({
     <html lang="ja">
       <body className="min-h-screen bg-sky-50">
         {/* ★ ここに AdSense スクリプトを「そのまま」書く */}
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            background: "red",
-            color: "white",
-            padding: "5px",
-            zIndex: 99999,
-          }}
-        >
-          LAYOUT-DEBUG
-        </div>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9264651168388030"
